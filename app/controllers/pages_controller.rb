3f7c088d4947a55fcb9d69d7f01175445cc9d7b6
Faizan Haider <hfaizan53@gmail.com>
2022-06-14 00:00:57 +0500

    Spine360

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  def index
+  end
+  def terms_condition
+  end
+  def privacy_policy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
+  get "/privacy-policy", to:"pages#privacy_policy"
+  get "/terms-and-condition", to:"pages#terms_condition"
 end
